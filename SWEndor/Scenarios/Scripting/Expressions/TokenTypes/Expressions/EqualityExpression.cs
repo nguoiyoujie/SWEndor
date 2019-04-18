8755cbdda5a047804a85bf049b5d8971cce2a075
youjie <yj_nguoi@transcendt.com>
2019-04-18 10:22:39 +0800

    Scripting: support expression 'a != b'

--- SWEndor/Scenarios/Scripting/Expressions/TokenTypes/Expressions/EqualityExpression.cs
+++ SWEndor/Scenarios/Scripting/Expressions/TokenTypes/Expressions/EqualityExpression.cs
@@ -5,12 +5,14 @@ namespace SWEndor.Scenarios.Scripting.Expressions.TokenTypes.Expressions
 {
   public class EqualityExpression : CExpression
   {
+    private bool isUnequal = false;
     private CExpression _first;
     private ThreadSafeList<CExpression> _set = new ThreadSafeList<CExpression>();
 
     internal EqualityExpression(Lexer lexer) : base(lexer)
     {
       // RELATEEXPR == RELATEEXPR ...
+      // RELATEEXPR != RELATEEXPR ...
 
       _first = new RelationalExpression(lexer).Get();
 
@@ -19,6 +21,13 @@ namespace SWEndor.Scenarios.Scripting.Expressions.TokenTypes.Expressions
         )
       {
         lexer.Next();
+        _set.Add(new RelationalExpression(lexer).Get());
+      }
+      else if (_type == TokenEnum.NOTEQUAL // !=
+      )
+      {
+        lexer.Next();
+        isUnequal = true;
         _set.Add(new RelationalExpression(lexer).Get());
       }
     }
@@ -36,7 +45,10 @@ namespace SWEndor.Scenarios.Scripting.Expressions.TokenTypes.Expressions
       foreach (CExpression _expr in _set.GetList())
       {
         dynamic adden = _expr.Evaluate(context);
-        try { result = _first.Evaluate(context) == adden; } catch (Exception ex) { throw new EvalException("==", result, adden, ex); }
+        if (isUnequal)
+          try { result = _first.Evaluate(context) != adden; } catch (Exception ex) { throw new EvalException("!=", result, adden, ex); }
+        else
+          try { result = _first.Evaluate(context) == adden; } catch (Exception ex) { throw new EvalException("==", result, adden, ex); }
       }
       return result;
     }
