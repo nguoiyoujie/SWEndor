3fbf7aed84b2d53edb47c9d9f489b3ba9b661334
youjie <yj_nguoi@transcendt.com>
2019-10-05 19:22:11 +0800

    shader edits

--- SWEndor/Build.cs
+++ SWEndor/Build.cs
@@ -1,1 +1,1 @@
-namespace SWEndor { public static class Build { public static string Time = "Sat 10/05/2019 18:44:23.89";}} 
+namespace SWEndor { public static class Build { public static string Time = "Sat 10/05/2019 19:14:14.33";}} 
