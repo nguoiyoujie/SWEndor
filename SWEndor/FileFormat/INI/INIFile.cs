9763bb51f82de08b704ea5fb464e2de38247b1eb
youjie <yj_nguoi@transcendt.com>
2019-10-18 22:00:21 +0800

    minor INIFile optimization

--- SWEndor/FileFormat/INI/INIFile.cs
+++ SWEndor/FileFormat/INI/INIFile.cs
@@ -17,7 +17,7 @@ namespace SWEndor.FileFormat.INI
 
     public readonly string FilePath;
     private readonly Dictionary<string, INISection> m_sections = new Dictionary<string, INISection>();
-    public IEnumerable<string> Sections { get { return m_sections.Keys; } }
+    public Dictionary<string, INISection>.KeyCollection Sections { get { return m_sections.Keys; } }
 
     public void Reset()
     {
