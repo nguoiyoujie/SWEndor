f776737d732beb00c93daf853e0b3a5e608e137f
youjie <yj_nguoi@transcendt.com>
2019-11-02 11:20:03 +0800

    build

--- SWEndor/Build.cs
+++ SWEndor/Build.cs
@@ -1,1 +1,1 @@
-namespace SWEndor { public static class Build { public static string Time = "Wed 10/30/2019 20:39:58.08";}} 
+namespace SWEndor { public static class Build { public static string Time = "Sat 11/02/2019  3:15:40.11";}} 
