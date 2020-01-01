e008feb1e6416797917655a89b96d8423b3208b1
youjie <yj_nguoi@transcendt.com>
2020-01-02 00:07:33 +0800

    Formalize Fody/Costura nuget. Move main project to SWEndor.Core. Build.

---
+++ src/SWEndor.Core/Core/Init.cs
@@ -0,0 +1,40 @@
+﻿using Primrose.Primitives.Extensions;
+using SWEndor.Input.Functions;
+using System;
+using System.Windows.Forms;
+
+namespace SWEndor.Core
+{
+  public static class Init
+  {
+    public static void Execute()
+    {
+      try
+      {
+        Log.Write(Log.DEBUG, LogType.SYS_INIT, Application.ProductName, Globals.Version);
+
+        Globals.PreInit();
+        Engine engine = Globals.InitEngine();
+
+        InputFunction.Registry.GenerateDefault();
+        Settings.LoadSettings(engine);
+        Settings.SaveSettings(engine);
+
+        Application.Run(new UI.Forms.BackgroundForm(engine));
+
+        engine.Dispose();
+      }
+      catch (Exception ex)
+      {
+        Log.WriteErr(Log.INITERROR, ex);
+        MessageBox.Show(TextLocalization.Get(TextLocalKeys.SYSTEM_INIT_ERROR).F(Log.INITERROR)
+                      , TextLocalization.Get(TextLocalKeys.SYSTEM_TITLE_ERROR).F(Application.ProductName)
+                      , MessageBoxButtons.OK);
+      }
+      finally
+      {
+        Log.Write(Log.DEBUG, LogType.SYS_CLOSE, Application.ProductName);
+      }
+    }
+  }
+}
--- src/SWEndor/Program.cs
+++ src/SWEndor/Program.cs
@@ -1,6 +1,4 @@
 ﻿using SWEndor.Core;
-using SWEndor.Input.Functions;
-using Primrose.Primitives.Extensions;
 using System;
 using System.Windows.Forms;
 
@@ -8,45 +6,12 @@ namespace SWEndor
 {
   static class Program
   {
-    /// <summary>
-    /// The main entry point for the application.
-    /// </summary>
     [STAThread]
     static void Main()
     {
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
-      Init();
-    }
-
-    static void Init()
-    {
-      try
-      {
-        Log.Write(Log.DEBUG, LogType.SYS_INIT, Application.ProductName, Globals.Version);
-
-        Globals.PreInit();
-        Engine engine = Globals.InitEngine();
-
-        InputFunction.Registry.GenerateDefault();
-        Settings.LoadSettings(engine);
-        Settings.SaveSettings(engine);
-
-        Application.Run(new UI.Forms.BackgroundForm(engine));
-
-        engine.Dispose();
-      }
-      catch (Exception ex)
-      {
-        Log.WriteErr(Log.INITERROR, ex);
-        MessageBox.Show(TextLocalization.Get(TextLocalKeys.SYSTEM_INIT_ERROR).F(Log.INITERROR)
-                      , TextLocalization.Get(TextLocalKeys.SYSTEM_TITLE_ERROR).F(Application.ProductName)
-                      , MessageBoxButtons.OK);
-      }
-      finally
-      {
-        Log.Write(Log.DEBUG, LogType.SYS_CLOSE, Application.ProductName);
-      }
+      Init.Execute();
     }
   }
 }
